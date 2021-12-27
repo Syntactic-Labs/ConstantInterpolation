07a263f8b4b94faed9abe3a9505a024abeb72fff
Michael W <walkerrepair255@yahoo.com>
2021-12-26 20:10:16 -0500

    Add project files.

---
+++ ConstantInterpolation/Program.cs
@@ -0,0 +1,11 @@
+﻿const string companyName = "Acme";
+const string productName = $"{companyName} Anvils";
+const string productDescription = $"{productName} are the best way to crush roadrunners.";
+
+Console.WriteLine(productDescription);
+
+[Obsolete($"This is no longer used for {productName}")]
+void SayHi()
+{
+
+}
